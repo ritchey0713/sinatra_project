4f7d02801ec93f723d63d5c9ff3712c530176609
ritchey0713 <ritchey0713@gmail.com>
2018-05-27 19:06:37 -0400

    added new destinations

--- rider_log/app/controllers/destination_controller.rb
+++ rider_log/app/controllers/destination_controller.rb
@@ -1,79 +1,79 @@
 class DestinationController < ApplicationController
 
-  get '/destinations' do 
+  get '/destinations' do
     if logged_in?
-      @destinations = Destination.all 
+      @destinations = Destination.all
       erb :'/destinations/destinations'
-    else 
+    else
       redirect '/login'
-    end 
-  end 
+    end
+  end
 
-  get '/destinations/new' do 
-    if logged_in? 
+  get '/destinations/new' do
+    if logged_in?
       erb :'/destinations/new_destination'
-    else 
+    else
       redirect "/login"
-    end 
-  end 
+    end
+  end
 
   post '/destinations' do
     if logged_in?
       if params[:name] == ""
         # flash[:message] = "Name cannot be blank!"
         redirect '/destinations/new_destination'
-      else 
-        @destination = current_user.destinations.build(name: params[name])
-        @destination.save 
+      else
+        @destination = current_user.destinations.build(name: params[:name], distance: params[:distance])
+        @destination.save
         redirect "/destinations/#{@destination.id}"
-      end  
-    else 
-      redirect "/login"      
-    end  
-  end 
+      end
+    else
+      redirect "/login"
+    end
+  end
 
-  get '/destinations/:id' do 
-    if logged_in? 
+  get '/destinations/:id' do
+    if logged_in?
       @destination = Destination.find_by(params[:id])
       erb :"/destinations/show_destination"
-    else 
+    else
       redirect "/login"
-    end 
+    end
   end
- 
-  get '/destinations/:id/edit' do 
-    if logged_in? 
-      @destination = Destination.find_by(params[:id])  
-      if @destination && @destination.rider == current_rider 
+
+  get '/destinations/:id/edit' do
+    if logged_in?
+      @destination = Destination.find_by(params[:id])
+      if @destination && @destination.rider == current_rider
         erb :"/destination/edit_destination"
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  patch '/destinations/:id' do 
+  patch '/destinations/:id' do
     @destination = Destination.find_by(params[:id])
-    if logged_in? 
-      if !params[:name] =="" && !params[:distance] =="" 
+    if logged_in?
+      if !params[:name] =="" && !params[:distance] ==""
         @destination.update(name: params[:name], distance: params[:distance])
-      else 
+      else
         redirect "/destinations/#{@desination.id}/edit"
-      end 
-    else 
+      end
+    else
       redirect "/login"
-    end 
+    end
 
-  end 
+  end
 
   delete '/destinations/:id/delete' do
-    if logged_in? 
+    if logged_in?
       @destination = Destination.find_by(params[:id])
-      @destination.delete 
+      @destination.delete
       redirect "/destinations"
-    else 
+    else
       redirect "/login"
-    end  
-  end 
+    end
+  end
 
 
 
-end 
+end
